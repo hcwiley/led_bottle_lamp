f2c280b43728ea20f61fa30e46f19294e1c81250
H. Cole Wiley <hcwiley@gmail.com>
2013-09-16 16:55:15 -0500

    good test on pi

--- rgb_lamp/rgb_lamp.ino
+++ rgb_lamp/rgb_lamp.ino
@@ -10,20 +10,31 @@ void setup()
   pinMode(GREENPin, OUTPUT);
   pinMode(BLUEPin, OUTPUT);
   Serial.begin(9600);
+  while( Serial.available() == 0 ){}
+  Serial.read();
+  Serial.print("hey");
 }
 
+int ser_buf = 0;
 void loop()
 {
-
-  while(!Serial.available()){}
+  while( (ser_buf = Serial.available()) == 0 ){}
   char dir = Serial.read();
+  ser_buf--;
+  Serial.print("ser buf ");
+  Serial.println(ser_buf);
+  while(ser_buf > 0){
+    Serial.read();
+    ser_buf--;
+  }
   // if its c, clear the LEDs. well make it blue...
   if(dir == 'c'){
     analogWrite(REDPin, 0);
     analogWrite(GREENPin, 0);
-    analogWrite(BLUEPin, 120);
+    analogWrite(BLUEPin, 255);
   }
   else if(dir == 'n'){
+    delay(2);
     while(!Serial.available()){}
     int count = (int)(char)Serial.read();
     int pow = map(count, 0, 10, 0, 255);
